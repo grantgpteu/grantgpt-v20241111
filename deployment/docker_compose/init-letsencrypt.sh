d21345649c5336f99f7695fe1d7c4283850357e9
a8142 <alex.kitanidis@gmail.com>
2024-11-11 18:35:23 +0100

    feat: wip

--- deployment/docker_compose/init-letsencrypt.sh
+++ deployment/docker_compose/init-letsencrypt.sh
@@ -22,23 +22,23 @@ docker_compose_cmd() {
 COMPOSE_CMD=$(docker_compose_cmd)
 
 # Only add www to domain list if domain wasn't explicitly set as a subdomain
-if [[ ! $DOMAIN == www.* ]]; then
-    domains=("$DOMAIN" "www.$DOMAIN")
-else
-    domains=("$DOMAIN")
-fi
+#if [[ ! $DOMAIN == www.* ]]; then
+#    domains=("$DOMAIN" "www.$DOMAIN")
+#else
+#    domains=("$DOMAIN")
+#fi
 
 rsa_key_size=4096
 data_path="../data/certbot"
 email="$EMAIL" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
-if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-    exit
-  fi
-fi
+#if [ -d "$data_path" ]; then
+#  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+#  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+#    exit
+#  fi
+#fi
 
 
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
